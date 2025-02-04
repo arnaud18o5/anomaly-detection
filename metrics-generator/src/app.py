5852c5236973d978b4c59ac1c260f4320160940c
Arnaud Avocat Gros <arnaud.avocatgros18@gmail.com>
2025-02-04 08:45:09 +0100

    setup basic alerting with statistical analysis

--- metrics-generator/src/app.py
+++ metrics-generator/src/app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask
 from prometheus_client import start_http_server, Gauge
 import csv
@@ -9,13 +10,15 @@ app = Flask(__name__)
 metric_gauge = Gauge('metric_value', 'Description of metric_value')
 
 def read_csv_and_update_metrics():
-    with open('sample_data/metrics.csv', mode='r') as file:
-        csv_reader = csv.DictReader(file)
-        for row in csv_reader:
-            timestamp = row['timestamp']
-            metric_value = float(row['metric_value'])
-            metric_gauge.set(metric_value)
-            time.sleep(1)  # Simulate real-time data
+    while True:
+        with open('sample_data/metrics.csv', mode='r') as file:
+            csv_reader = csv.DictReader(file)
+            for row in csv_reader:
+                timestamp = row['timestamp']
+                metric_value = float(row['metric_value'])
+                offset = random.gauss(0, 10)  # Gaussian distribution with mean 0 and standard deviation 5
+                metric_gauge.set(metric_value + offset)
+                time.sleep(1)  # Simulate real-time data
 
 @app.route('/')
 def hello_world():
